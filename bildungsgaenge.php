bc3a6056c5211b1859f20341b077a58fa9740622
Nico Loecker <nicolas.affeln@gmail.com>
2020-03-11 19:29:15 +0100

    bildungsgang delete-func hinzugefügt

--- bildungsgaenge.php
+++ bildungsgaenge.php
@@ -4,13 +4,14 @@
 
 	if(isset($_GET['bg_id']) && isset($_GET['action']))
 	{
-		echo $_GET['bg_id'];
-		echo $_GET['action'];
+		//echo $_GET['bg_id'];
+		//echo $_GET['action'];
 		if($_GET['action']=="delete")
 		{
 			$sql="delete from bildungsgaenge where bg_id = ".$_GET['bg_id'];
-			echo $sql;
+			//echo $sql;
 			$result = mysqli_query($db, $sql);
+			header("Location: ?page=bildungsgaenge");
 		}
 	}
 
@@ -36,7 +37,7 @@
 				<td>
 					<a href='?action=view&bg_id=".$satz['bg_id']."'><img src='icons/view.png' width=15></a>
 					<a href='?action=edit&bg_id=".$satz['bg_id']."'><img src='icons/bearbeiten.png' width=15></a>
-					<a href='?action=delete&bg_id=".$satz['bg_id']."'><img src='icons/loeschen.png' width=15></a>
+					<a href='?page=bildungsgaenge&action=delete&bg_id=".$satz['bg_id']."'><img src='icons/loeschen.png' width=15></a>
 				</td>
 			  </tr>";
 
