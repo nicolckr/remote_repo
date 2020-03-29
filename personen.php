e72b788d9edd652c69c6d9b50692d00285f28549
Nico Loecker <nicolas.affeln@gmail.com>
2020-03-29 20:49:44 +0200

    Versuch, neue person anzulegen

--- personen.php
+++ personen.php
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function ()
 
 	require_once('config/db.php');				//Fügt hier den Inhalt der Datei db.php ein
 
-	if(isset($_GET['p_id']) && isset($_GET['action']))
+	if(isset($_GET['p_id']) || isset($_GET['new_id']) && isset($_GET['action']))
 	{
 		/*echo $_GET['p_id'];
 		echo $_GET['action'];*/
@@ -177,55 +177,126 @@ document.addEventListener('DOMContentLoaded', function ()
 					</form>";
 			}
 		}
+		elseif($_GET['action']=="add")
+		{
+			$sql2="	SELECT klassen.*				
+					FROM klassen
+					ORDER BY k_name";
+				
+			$result2 = mysqli_query($db, $sql2);
+			$kls = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+
+			echo "<h3>Neue Person anlegen</h3>";	
+				echo "<hr>";
+				echo "<form method=post>
+		
+						<table>
+							<tr>
+								<td>Nachname:</td>
+								<td><input type='text' name='name' size=30></td>
+							</tr>
+							<tr>
+								<td>Vorname:</td>
+								<td><input type='text' name='vname' size=30></td>
+							</tr>
+							<tr>
+								<td>User:</td>
+								<td><input type='text' name='new_user' size=30></td>
+							</tr>
+							<tr>
+								<td>E-Mail:</td>
+								<td><input type='text' name='mail' size=30></td>
+							</tr>
+							<tr>
+								<td>Passwort:</td>								
+								<td><input type='password' name='pass' size=30></td>								
+							</tr>
+							<tr>
+								<td>Klasse:</td>
+								<td><select name='k_id'>";       
+								foreach ($kls as $kl)
+								{
+									if($kl['k_id']==$data['k_id'])
+									{
+										echo "<option selected value='".$kl['k_id']."'>".$kl['k_name']."</option>";
+									}
+									else
+									{
+										echo "<option value='".$kl['k_id']."'>".$kl['k_name']."</option>";
+									}
+								}
+
+				echo "			</select> </td>
+							</tr>
+						</table>
+						<input type='submit' value='Speichern'>";
+						
+
+			$name = $_POST['name'];
+			$vname = $_POST['vname'];
+			$new_user = $_POST['new_user'];
+			$mail = $_POST['mail'];
+			$pass = $_POST['pass'];
+			$k_id = $_POST['k_id'];
+
+			echo "</form>";
+
+			$query = "SELECT MAX(p_id) FROM personen";
+			$result = mysqli_query($db, $query);
+			$row = mysqli_fetch_row($result);
+			$p_id = $row[0]+1;
+
+			$sql ="	INSERT INTO personen (p_id, k_id, p_name, p_vname, p_user, p_pass, p_mail)
+			 		VALUES ($p_id, $k_id, $name, $vname, $new_user, $mail, $pass)";
+			$res=mysqli_query($db,$sql);
+
+			
+		}
 	}
 	else
 	{
-	
 		$sql = "SELECT * from personen left join klassen using(k_id) left join bildungsgaenge using(bg_id) order by p_name";
 		$result = mysqli_query($db, $sql);		//sql-Abfrage wird in die Variable $result übergeben
 
 		$data = mysqli_fetch_all($result,MYSQLI_ASSOC);			//alle Zeilen der Abfrage, die in $result stehen, werden als 2-dim, assoziatives Array, in die Variable $data übergeben
 
-			echo "<h1>Personen</h1>";
+		echo "<h1>Personen</h1>";
 
 		echo "<hr>";
-			echo "<table border=2>";						//Tabelle mit der Headerzeile
-		echo "<tr>
-							<th>p_name</th>
-							<th>p_vname</th>
-							<th>p_user</th>
-							<th>bg_name</th>
-							<th>k_name</th>
-							<th>action</th>
-				</tr>";
-		//mysqli_data_seek($result,0);
-		//while($satz = mysqli_fetch_array($result,MYSQLI_ASSOC))
-			foreach($data as $satz)		//aus dem 2-dim, assoziativen Array, werden die Abfrageergebnisse aufgeteilt. Also je nachdem, welches Array (Indize) angesprochen wird. Solange die Variable $data noch ein neues Ergebnis übergibt, läuft die foreach-Schleife
+		echo "<table border=2>";						//Tabelle mit der Headerzeile
+		echo "		<tr>
+						<th>p_name</th>
+						<th>p_vname</th>
+						<th>p_user</th>
+						<th>bg_name</th>
+						<th>k_name</th>
+						<th>action</th>
+			  		</tr>";
+		
+		foreach($data as $satz)		//aus dem 2-dim, assoziativen Array, werden die Abfrageergebnisse aufgeteilt. Also je nachdem, welches Array (Indize) angesprochen wird. Solange die Variable $data noch ein neues Ergebnis übergibt, läuft die foreach-Schleife
 		{
-			echo "<tr>
-					<td>".$satz['p_name']."</td>
-					<td>".$satz['p_vname']."</td>
-					<td>".$satz['p_user']."</td>
-					<td>".$satz['bg_name']."</td>
-					<td>".$satz['k_name']."</td>
-					<td>
-						<a href='?page=personen&action=view&p_id=".$satz['p_id']."'><img src='icons/view.png' width=15></a>
-						<a href='?page=personen&action=edit&p_id=".$satz['p_id']."'><img src='icons/bearbeiten.png' width=15></a>
-						<a href='?page=personen&action=delete&p_id=".$satz['p_id']."'><img src='icons/loeschen.png' width=15></a>
-					</td>
-
-				</tr>";
-
-			/* foreach($satz as $feld)
-			{
-				echo "<td>";
-				echo "$feld | ";
-				echo "
-			}
-			*/
-			
+			echo "	<tr>
+						<td>".$satz['p_name']."</td>
+						<td>".$satz['p_vname']."</td>
+						<td>".$satz['p_user']."</td>
+						<td>".$satz['bg_name']."</td>
+						<td>".$satz['k_name']."</td>
+						<td>
+							<a href='?page=personen&action=view&p_id=".$satz['p_id']."'><img src='icons/view.png' width=15></a>
+							<a href='?page=personen&action=edit&p_id=".$satz['p_id']."'><img src='icons/bearbeiten.png' width=15></a>
+							<a href='?page=personen&action=delete&p_id=".$satz['p_id']."'><img src='icons/loeschen.png' width=15></a>
+						</td>
+				  	</tr>";
 		}
-		echo "</table>";
+		echo "</table><br>";
+		
+		//Die höchste ID aus 'personen' abfragen
+		$query = "SELECT MAX(p_id) FROM personen";
+		$result = mysqli_query($db, $query);
+		$row = mysqli_fetch_row($result);
+		$id = $row[0]+1;
+
+		echo "<a href='?page=personen&action=add&new_id=".$id."'>Neue Person anlegen</a>";
 	}
 
 ?>
