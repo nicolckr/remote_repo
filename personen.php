749de4f9bedab486d0094fdc2715c05f82db39b6
Nico Loecker <nicolas.affeln@gmail.com>
2020-04-01 20:29:08 +0200

    Passwort erfolgreich verschlüsselt, add-Funktion verbessert

--- personen.php
+++ personen.php
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function ()
 	{
 		if(document.getElementById('p_pass').type == 'password')
 		{
-			document.getElementById('p_pass').type == 'text';
+			document.getElementById('p_pass').type = 'text';
 			this.innerText = 'Passwort verstecken';
 		}
 		else
 		{
 			document.getElementById('p_pass').type = 'password';
-			this.innerText = 'Passwort sichtbar machen';
+			this.innerText = 'Passwort anzeigen';
 		}
 	});
 });
@@ -32,15 +32,15 @@ document.addEventListener('DOMContentLoaded', function ()
 
 	if(isset($_GET['p_id']) || isset($_GET['new_id']) && isset($_GET['action']))
 	{
-		/*echo $_GET['p_id'];
-		echo $_GET['action'];*/
+		//Action: Delete
 		if($_GET['action']=="delete")
 		{
 			$sql="delete from personen where p_id = ".$_GET['p_id'];
-			//echo "<br>". $sql;
 			$result = mysqli_query($db, $sql);
 			header("Location: ?page=personen");
 		}
+
+		//Action: View
 		elseif($_GET['action']=="view")
 		{
 			$sql="SELECT * 
@@ -87,6 +87,8 @@ document.addEventListener('DOMContentLoaded', function ()
 
 			<a href='?page=personen'>Zurück zu allen Personen</a> ";
 		}
+
+		//Action: Edit
 		elseif($_GET['action']=="edit")
 		{
 			if(isset($_POST['p_name']))
@@ -96,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function ()
 					  	  p_vname='".$_POST['p_vname']."',
 						  p_user='".$_POST['p_user']."',
 						  p_mail='".$_POST['p_mail']."',
-						  k_id =".$_POST['k_id'];
+						  k_id = ".$_POST['k_id'];
 				if($_POST['p_pass']<>"Dein Passwort")
 				{
 					$sql.=",p_pass=md5('".$_POST['p_pass']."')";
@@ -152,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function ()
 								<td>Passwort:</td>
 								<td>
 									<input type='password' name='p_pass' id='p_pass' required autocomplete='off' minlength=8 maxlength=20 value='Dein Passwort' size=30>
-									<button id='check' type='button'>Passwort sichtbar machen</button>
+									<button id='check' type='button'>Passwort anzeigen</button>
 								</td>
 							</tr>
 							<tr>
@@ -170,87 +172,94 @@ document.addEventListener('DOMContentLoaded', function ()
 									}
 								}
 
-				echo "			</select> </td>
+				echo "			</select></td>
 							</tr>
 						</table>
 						<input type='submit' value='Speichern'>
 					</form>";
 			}
 		}
+
+		//Action: Add
 		elseif($_GET['action']=="add")
 		{
-			$sql2="	SELECT klassen.*				
-					FROM klassen
-					ORDER BY k_name";
+			if(isset($_POST['name']))
+			{
+				$name = $_POST['name'];
+				$vname = $_POST['vname'];
+				$new_user = $_POST['new_user'];
+				$mail = $_POST['mail'];
+				$new_pass = $_POST['pass'];
+				$k_id = $_POST['k_id'];
+	
+				$query = "SELECT MAX(p_id) FROM personen";
+				$result = mysqli_query($db, $query);
+				$row = mysqli_fetch_row($result);
+				$p_id = $row[0]+1;
+	
+				$sql = "INSERT INTO personen (p_id, k_id, p_name, p_vname, p_user, p_pass, p_mail)
+						VALUES ($p_id, $k_id, $name, $vname, $new_user, $mail, $new_pass)";
 				
-			$result2 = mysqli_query($db, $sql2);
-			$kls = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+				$res = mysqli_query($db,$sql);
+				echo "<br><br>Personen angelegt: ".mysqli_affected_rows($db)."<br>";
+				echo"<a href='?page=personen'>zurück zu den Personen</a>";
+	
+			}
+			else
+			{
+				$sql2="	SELECT klassen.*				
+						FROM klassen
+						ORDER BY k_name";
+					
+				$result2 = mysqli_query($db, $sql2);
+				$kls = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
-			echo "<h3>Neue Person anlegen</h3>";	
-				echo "<hr>";
-				echo "<form method=post>
-		
-						<table>
-							<tr>
-								<td>Nachname:</td>
-								<td><input type='text' name='name' size=30></td>
-							</tr>
-							<tr>
-								<td>Vorname:</td>
-								<td><input type='text' name='vname' size=30></td>
-							</tr>
-							<tr>
-								<td>User:</td>
-								<td><input type='text' name='new_user' size=30></td>
-							</tr>
-							<tr>
-								<td>E-Mail:</td>
-								<td><input type='text' name='mail' size=30></td>
-							</tr>
-							<tr>
-								<td>Passwort:</td>								
-								<td><input type='password' name='pass' size=30></td>								
-							</tr>
-							<tr>
-								<td>Klasse:</td>
-								<td><select name='k_id'>";       
-								foreach ($kls as $kl)
-								{
-									if($kl['k_id']==$data['k_id'])
-									{
-										echo "<option selected value='".$kl['k_id']."'>".$kl['k_name']."</option>";
-									}
-									else
-									{
-										echo "<option value='".$kl['k_id']."'>".$kl['k_name']."</option>";
-									}
-								}
-
-				echo "			</select> </td>
-							</tr>
-						</table>
-						<input type='submit' value='Speichern'>";
-						
-
-			$name = $_POST['name'];
-			$vname = $_POST['vname'];
-			$new_user = $_POST['new_user'];
-			$mail = $_POST['mail'];
-			$pass = $_POST['pass'];
-			$k_id = $_POST['k_id'];
-
-			echo "</form>";
-
-			$query = "SELECT MAX(p_id) FROM personen";
-			$result = mysqli_query($db, $query);
-			$row = mysqli_fetch_row($result);
-			$p_id = $row[0]+1;
-
-			$sql ="	INSERT INTO personen (p_id, k_id, p_name, p_vname, p_user, p_pass, p_mail)
-			 		VALUES ($p_id, $k_id, $name, $vname, $new_user, $mail, $pass)";
-			$res=mysqli_query($db,$sql);
-
+				echo "<h3>Neue Person anlegen</h3>";	
+					echo "<hr>";
+					echo "<form method=post>
 			
+							<table>
+								<tr>
+									<td>Nachname:</td>
+									<td><input type='text' name='name' size=30></td>
+								</tr>
+								<tr>
+									<td>Vorname:</td>
+									<td><input type='text' name='vname' size=30></td>
+								</tr>
+								<tr>
+									<td>User:</td>
+									<td><input type='text' name='new_user' size=30></td>
+								</tr>
+								<tr>
+									<td>E-Mail:</td>
+									<td><input type='text' name='mail' size=30></td>
+								</tr>
+								<tr>
+									<td>Passwort:</td>								
+									<td><input type='password' name='pass' size=30></td>								
+								</tr>
+								<tr>
+									<td>Klasse:</td>
+									<td><select name='k_id'>";       
+									foreach ($kls as $kl)
+									{
+										if($kl['k_id']==$data['k_id'])
+										{
+											echo "<option selected value='".$kl['k_id']."'>".$kl['k_name']."</option>";
+										}
+										else
+										{
+											echo "<option value='".$kl['k_id']."'>".$kl['k_name']."</option>";
+										}
+									}
+
+					echo "			</select> </td>
+								</tr>
+							</table>
+							<input type='submit' value='Speichern'>";
+					echo "</form>";	
+			}	
 		}
 	}
 	else
