a6e1267c23dfc39df6d404fb6cbc48aa4116286b
Nico Loecker <nicolas.affeln@gmail.com>
2020-05-07 14:05:34 +0200

    Impressum erstellt

--- views/impressum.php
+++ views/impressum.php
@@ -1,13 +1,13 @@
 <h1>Impressum</h1>
-
+<p>Informationspflicht laut § 5 TMG.</p>
+<p>MusterFirma<br />In der Ecke 10, a, <br />58809 Neuenrade, <br />Deutschland</p>
 <p>
-  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-  aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-  duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-  sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-  consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-  ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-  eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-  gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</p>
+<p>
+<strong>Tel.:</strong> 0175 8335447<br />
+<strong>E-Mail:</strong> <a href="mailto:nicolas.affeln@gmail.com">nicolas.affeln@gmail.com</a>
+</p>
+<p>
+<strong>Berufsbezeichnung:</strong> Code Developer</p>
+<p style="margin-top:15px;">Quelle: Erstellt mit dem <a title="Impressum Generator Deutschland" href="https://www.adsimple.de/impressum-generator/" target="_blank" rel="follow" style="text-decoration:none;">Impressum Generator</a> von AdSimple in Kooperation mit <a href="https://www.warkly.de" target="_blank" rel="follow" title="">warkly.de</a>
 </p>
